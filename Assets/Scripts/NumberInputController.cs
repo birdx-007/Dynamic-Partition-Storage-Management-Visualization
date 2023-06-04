d45a6a8ac9b03fadad9faf8dd22fbba8bcae1eb7
刘浩然 <1042742166@qq.com>
2023-06-04 21:35:14 +0800

    20230604: add Number Input Panel

---
+++ Assets/Scripts/NumberInputController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberInputController : MonoBehaviour
+{
+    public InputField inputField;
+    public int number;
+    public void UpdateNumber()
+    {
+        int.TryParse(inputField.text,out number);
+    }
+    public void OnNumberButtonPressed(Text buttonText)
+    {
+        if (inputField.text.Length > 0 || buttonText.text != "0")
+        {
+            inputField.text += buttonText.text;
+            UpdateNumber();
+        }
+    }
+    public void OnBackspaceButtonPressed()
+    {
+        if(inputField.text.Length > 0)
+        {
+            inputField.text = inputField.text.Remove(inputField.text.Length - 1);
+            UpdateNumber();
+        }
+    }
+}
